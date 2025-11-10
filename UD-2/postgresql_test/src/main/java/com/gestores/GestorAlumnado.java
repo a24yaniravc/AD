caf22ac35a23661ccdbb2fb6559a2c294c515e9e
a24yaniravc <a24yaniravc@iessanclemente.net>
2025-11-10 11:15:51 +0100

    Commit

--- UD-2/postgresql_test/src/main/java/com/gestores/GestorAlumnado.java
+++ UD-2/postgresql_test/src/main/java/com/gestores/GestorAlumnado.java
@@ -1,11 +1,27 @@
 package com.gestores;
 
 import java.sql.Statement;
-
+import java.text.ParseException;
+import java.util.Scanner;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/*
+ * id
+ * nombre
+ * edad
+ * email
+ * añadir alumno
+ * borrar alumno
+ * modificar
+ * consultar todos los alumnos
+ */
+
+
+
 public class GestorAlumnado {
+    private Scanner sc = new Scanner(System.in);
     private Connection connection;
 
     public GestorAlumnado(Connection conn) {
@@ -14,12 +30,56 @@ public class GestorAlumnado {
 
     public void crearTablaAlumnos() {
         // Lógica para crear la tabla de alumnos
-        String sql = "CREATE TABLE IF NOT EXISTS alumnos (id SERIAL PRIMARY KEY, nombre VARCHAR(100), edad INT)";
+        String sql = "CREATE TABLE IF NOT EXISTS alumnos (id SERIAL PRIMARY KEY, nombre VARCHAR(100), edad INT, email VARCHAR(100))";
         try (Statement stmt = connection.createStatement()) {
             stmt.executeUpdate(sql);
             System.out.println("Tabla de alumnos creada con éxito.");
         } catch (SQLException e) {
             System.err.println("Error al crear la tabla de alumnos: " + e.getMessage());
+        }
+    }
+
+    public void anhadirAlumno() {
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+        System.out.print("Edad: ");
+        String edad = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        
+        String sql = "INSERT INTO alumnos(nombre, edad, email) VALUES (?, ?, ?)";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, nombre);
+            pstmt.setInt(2, Integer.parseInt(edad));
+            pstmt.setString(3, email);
+            pstmt.executeUpdate();
+            System.out.println("Alumno añadido con éxito.");
+        } catch (SQLException e) {
+            System.err.println("Error al añadir alumno: " + e.getMessage());
+        }
+    }
+
+    public void borrarAlumno(String id) {
+        String sql = "DELETE FROM alumnos WHERE id = '"+id+"'";
+
+        try (Statement stmt = connection.createStatement()) {
+            stmt.executeUpdate(sql);
+        } catch (SQLException e){
+            System.err.println("Error al eliminar alumno: " + e.getMessage());
+        }
+    }
+
+    public void modificarAlumno(String id) {
+        String sql = "";
+    }
+
+    public void listarAlumnado() {
+        String sql = "SELECT * FROM alumnos";
+        try (Statement stmt = connection.createStatement()) {
+            stmt.executeQuery(sql);
+
+        } catch (SQLException e) {
+            System.out.println("Error al mostrar a los alumnos: " + e.getMessage());
         }
     }
 
--- UD-2/postgresql_test/src/main/java/com/principales/Principal.java
+++ UD-2/postgresql_test/src/main/java/com/principales/Principal.java
@@ -1,6 +1,7 @@
 package com.principales;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 import com.conexiones.PostgreSQLConnection;
 import com.conexiones.PostgreSQL_DB;
@@ -8,6 +9,9 @@ import com.gestores.GestorAlumnado;
 import com.gestores.GestorBD;
 
 public class Principal {
+    private static Scanner sc = new Scanner(System.in);
+
+    // Variables
     private static PostgreSQLConnection postgres;
     private static PostgreSQL_DB DB;
     private static Connection conn;
@@ -35,6 +39,48 @@ public class Principal {
         // Creamos la tabla de alumnos
         gestorAlumnado = new GestorAlumnado(conn);
         gestorAlumnado.crearTablaAlumnos();
+
+        // Menú de opciones
+        String resp = "";
+        String r = "";
+
+        while (!resp.equals("5")) {
+            System.out.println("");
+            System.out.println("===== Menú =====");
+            System.out.println("1. Añadir alumno");
+            System.out.println("2. Eliminar alumno");
+            System.out.println("3. Modificar alumno");
+            System.out.println("4. Listar todos los alumnos");
+            System.out.println("5. Salir");
+            System.out.print("Selección: ");
+            resp = sc.nextLine();
+            System.out.println("");
+
+            switch (resp) {
+                case "1":
+                    System.out.println("> Añadir alumno seleccionado");
+                    gestorAlumnado.anhadirAlumno();
+                    break;
+                case "2":
+                    System.out.println("> Eliminar alumno seleccionado");
+                    System.out.print("Escriba la id del alumno a eliminar: ");
+                    r = sc.nextLine();
+                    gestorAlumnado.borrarAlumno(r);
+                    break;
+                case "3":
+                    System.out.println("> Modificar alumno seleccionado");
+                    System.out.print("Escriba la id del alumno a modificar: ");
+                    r = sc.nextLine();
+                    gestorAlumnado.modificarAlumno(r);
+                    break;
+                case "4":
+                    System.out.println("> Listar todos los alumnos seleccionado");
+                    gestorAlumnado.listarAlumnado();
+                default:
+                    System.out.println("Saliendo...");
+                    break;
+            }
+        }
 
         // Eliminar tabla de alumnos
         // gestorAlumnado.eliminarTablaAlumnos();
